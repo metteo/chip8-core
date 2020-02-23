9f724773bf39710571ad28b92f180462c292150b
metteo <grz3gorz.nowak@gmail.com>
2020-02-23 10:28:17 +0100

    Export gpu package to make ViewPort accessible, for now

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -14,6 +14,7 @@ module net.novaware.chip8.core {
 
     exports net.novaware.chip8.core.cpu;
     exports net.novaware.chip8.core.cpu.register; //TODO: internal, expose part as public API
+    exports net.novaware.chip8.core.gpu; //TODO: temporary, screen should not need internal ViewPort
     exports net.novaware.chip8.core.memory;
     exports net.novaware.chip8.core.util; //TODO: should be part of chip8-jvm or sth?
 }
