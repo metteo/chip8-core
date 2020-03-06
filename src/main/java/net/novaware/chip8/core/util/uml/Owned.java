42837f4a7dadd206d637db120e675ed89cf84f52
metteo <grz3gorz.nowak@gmail.com>
2020-03-06 11:40:27 +0100

    Add Owned.by param which clarifies usage

--- src/main/java/net/novaware/chip8/core/util/uml/Owned.java
+++ src/main/java/net/novaware/chip8/core/util/uml/Owned.java
@@ -9,4 +9,13 @@ import java.lang.annotation.*;
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Owned {
+
+    /**
+     * (Optional) The class that owns the field
+     *
+     * <p> Defaults to the type that stores the association.
+     *
+     * @return class which owns the target field
+     */
+    Class<?> by() default void.class;
 }
