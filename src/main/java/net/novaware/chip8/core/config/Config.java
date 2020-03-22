46a8e82e7aa1121b672a1e147fb2fccf890c390f
metteo <grz3gorz.nowak@gmail.com>
2020-03-22 20:35:59 +0100

    WIP: Project wide config interface

---
+++ src/main/java/net/novaware/chip8/core/config/Config.java
@@ -0,0 +1,6 @@
+package net.novaware.chip8.core.config;
+
+//TODO: move all configuration to this interface
+//TODO: provide immutable and mutable but threadsafe implementations
+public interface Config {
+}
