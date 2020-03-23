0dd947a59a6f38d33e6893fa99c824f7b5d7931e
metteo <grz3gorz.nowak@gmail.com>
2020-03-23 18:42:20 +0100

    Combine component configs

--- src/main/java/net/novaware/chip8/core/config/Config.java
+++ src/main/java/net/novaware/chip8/core/config/Config.java
@@ -1,6 +1,9 @@
 package net.novaware.chip8.core.config;
 
+import net.novaware.chip8.core.cpu.Cpu;
+import net.novaware.chip8.core.cpu.unit.ControlUnit;
+
 //TODO: move all configuration to this interface
 //TODO: provide immutable and mutable but threadsafe implementations
-public interface Config {
+public interface Config extends Cpu.Config, ControlUnit.Config {
 }
